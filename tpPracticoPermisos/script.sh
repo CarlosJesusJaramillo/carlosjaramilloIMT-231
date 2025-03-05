a47654519b623681285bf2cdba2633558ec90a98
CarlosJesusJaramillo <carlos.jaramillo@ucb.edu.b0>
2025-03-05 17:44:48 -0400

    Estos son los ejercicios para el practico Permisos.md

---
+++ tpPracticoPermisos/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ echo "Ingrese su Nombre de  Usuario"
+read nombreusuario
+echo "Hola mucho gusto " $nombreusuario ", Sea corcidalmene BIenvenido de nuevo"
+echo "Contenido del Script:"
+cat script.sh
