13eaaad3672e01089d4f7c5b8059f6084c399177
CarlosJesusJaramillo <carlos.jaramillo@ucb.edu.b0>
2025-03-07 22:53:45 -0400

    Este tesxto contiene la tarea practico de permisos en las que cada una tiene su descripcion de lo que se realizo

---
+++ tpPermisos/ejecutable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Este es un script ejecutable solo para el dueño, los demas no tienen acceso a este"
+echo "  Damos el permiso chmod 700 para dar permisos de lectura, escritura y ejecución para el dueño, y el valor 0 para el grupo y otros para que nadie más pueda acceder al archivo "
+
--- tpPracticoPermisos/script.sh
+++ tpPracticoPermisos/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  echo "Ingrese su Nombre de  Usuario"
 read nombreusuario
-echo "Hola mucho gusto " $nombreusuario ", Sea corcidalmene BIenvenido de nuevo"
+echo "Hola mucho gusto " $nombreusuario ", Sea cordialmene Bienvenido de nuevo al script"
 echo "Contenido del Script:"
 cat script.sh
